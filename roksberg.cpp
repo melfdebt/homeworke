40e23e4756bdb6a756f09d429eaa85a7ad66b3c8
kasirait <159796564+kasirait@users.noreply.github.com>
2024-02-17 17:01:45 +0300

    домашка с формулами до 24.02

---
+++ roksberg.cpp
@@ -0,0 +1,30 @@
+﻿// roksberg.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    //1
+    float r0,r1,r2,r3;
+    cin >> r1; cin >> r2; cin >> r3;
+    r0 = 1 / (1 / r1 + 1 / r2 + 1 / r3);
+    cout << r0;
+     //2
+    const float PI = 3.14;
+    float l,s,r;
+    cin >> l;
+    r = l /( 2 * PI);
+    s = PI * pow(r, 2);
+    cout << s;
+    //3
+    float v, t, a, s;
+    cin >> v; cin >> t; cin >> a;
+    s = v * t + (a * pow(t, 2))/ 2;
+    cout << s;
+
+}
+
+
