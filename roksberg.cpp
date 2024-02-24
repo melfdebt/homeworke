d711a0db76dd2865d7a98de03f15db905a057ca9
kasirait <159796564+kasirait@users.noreply.github.com>
2024-02-24 13:08:54 +0300

    дз до 29.02

--- roksberg.cpp
+++ roksberg.cpp
@@ -8,23 +8,42 @@ int main()
 {
     setlocale(LC_ALL, "rus");
     //1
-    float r0,r1,r2,r3;
-    cin >> r1; cin >> r2; cin >> r3;
-    r0 = 1 / (1 / r1 + 1 / r2 + 1 / r3);
-    cout << r0;
-     //2
-    const float PI = 3.14;
-    float l,s,r;
-    cin >> l;
-    r = l /( 2 * PI);
-    s = PI * pow(r, 2);
-    cout << s;
+   /* int sec,hours,min;
+    cin >> sec;
+    hours = sec / 3600;
+    sec = sec - (hours * 3600);
+    min = sec / 60;
+    sec = sec - (min * 60);
+    cout << hours << ":" << min << ":" << sec;*/
+    //2
+    /*float n;
+    int dollars, cents;
+    cin >> n;
+    dollars = n ;
+    n=n*100;
+    cents = n - (dollars * 100);
+    cout << dollars << " долларов " << cents << " центов";*/
     //3
-    float v, t, a, s;
-    cin >> v; cin >> t; cin >> a;
-    s = v * t + (a * pow(t, 2))/ 2;
-    cout << s;
-
+   /* int days, weeks;
+    cin >> days;
+    weeks = days / 7;
+    days = days - (weeks * 7);
+    cout << weeks << " weeks " << days << " days";*/
+    //4
+    cout << "Calculating running speed.";
+    float distance, time, speed;
+    int seconds, min,sec;
+    cout << "Enter the length of distance(meters) = ";cin >> distance;
+    cout << "Enter time(min.sec) ="; cin >> time;
+    cout << "distance: " << distance << "m"<<endl;
+    min = time;
+    sec = (time - min) * 100;
+    seconds = min * 60 + sec;
+    cout << "time: " << min << "min" << sec << "sec = " << seconds <<"seconds"<< endl;
+    distance = distance / 1000;
+    time = seconds / 3600;
+    speed = distance / time;
+    cout << "You were running at speed " << speed;
 }
 
 
