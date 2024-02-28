5962de4d7b6efde061c4b4f375b92d457e4ea253
kasirait <159796564+kasirait@users.noreply.github.com>
2024-02-28 22:30:26 +0300

    21.02
    дз про циклы 21.02

--- roksberg.cpp
+++ roksberg.cpp
@@ -8,49 +8,74 @@ int main()
 {
     setlocale(LC_ALL, "rus");
     //1
-    int sec,hours,min;
-    cin >> sec;
-    hours = sec / 3600;
-    sec = sec - (hours * 3600);
-    min = sec / 60;
-    sec = sec - (min * 60);
-    cout << hours << ":" << min << ":" << sec;
+    int num, a, b, count, count1;
+    cin >> num;
+    if (num >= 1000000 || num < 100000) cout << "error";
+    
+    else{
+    
+        count = 0;
+        count1 = 0;
+        for (int i = 100000; i != 100; i /= 10) {
+            a = num / i;
+            count += a;
+            num -= a * i;
+        for (int i = 100; i != 0; i /= 10) {
+        }
+            b = num / i;            count1 += b;
+            num -= b * i;
+        }
+        cout << ((count == count1) ? "счастливое число" : "нет");
+
+    }
     //2
-    float n;
-    int dollars, cents;
+   int n, first = 0, last = 0, count = 0;
+
     cin >> n;
-    dollars = n ;
-    n=n*100;
-    cents = n - (dollars * 100);
-    cout << dollars << " долларов " << cents << " центов";
-    //3
-    int days, weeks;
-    cin >> days;
-    weeks = days / 7;
-    days = days - (weeks * 7);
-    cout << weeks << " weeks " << days << " days";
+    for (int i = 1000; n > 0; i /= 100) {
+    if (n>1000 && n<10000){
+            first = n / i;
+            n -= first*i;
+            last = n / (i / 10);
+            n -= last*(i/10);
+            swap(first, last);
+            count += (first * i) + (last * (i / 10));
+        }
+        else cout<<"error";
+    }
+    cout << count;
+    //3 97-103
+    int a, count = 0;
+    cin >> a ;
+    for (int i = 1; i < 7; i++) {
+        cin >> a;
+        if (a > count) count = a;
+    }
+    cout << count;
     //4
-    cout << "Calculating running speed.";
-    float distance, time, speed;
-    int seconds, min,sec;
-    cout << "Enter the length of distance(meters) = ";cin >> distance;
-    cout << "Enter time(min.sec) ="; cin >> time;
-    cout << "distance: " << distance << "m"<<endl;
-    min = time;
-    sec = (time - min) * 100;
-    seconds = min * 60 + sec;
-    cout << "time: " << min << "min" << sec << "sec = " << seconds <<"seconds"<< endl;
-    distance = distance / 1000;
-    time = seconds / 3600;
-    speed = distance / time;
-    cout << "You were running at speed " << speed;
-    //5
-    float euros, percent,sum;
-    cout << "сумма в евро: "; cin >> euros;
-    cout << "процент годовых: "; cin >> percent;
-    percent = percent / 100;
-    sum = (euros * percent) / 12;
-    cout << "доход в месяц: " << sum << "евро";
+    float sab, sbc, m,pass=0,oil=0,need=0;
+    cin >> sab >> sbc >> m;
+    if (m < 2000 && m >= 0 ) {
+        if (m < 500) pass = 1;
+        else if (m >= 500 && m < 1000) pass = 4;
+        else if (m >= 1000 && m < 1500) pass = 7;
+        else if (m >= 1500 && m < 2000) pass = 9;
+        if (pass * sab > 300 || pass * sbc > 300) cout << "error";
+        else {
+            oil = 300 - (pass * sab);
+            need = sbc * pass;
+            if (oil <= need) {
+                need = need - oil;
+                cout << need;
+            }
+            else if (oil > need)cout << "достаточно топлива";
+        }
+    }
+    else cout << "перегруз";
+    
+
+    
+
 }
 
 
