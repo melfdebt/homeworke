bf548e98f9f4f939400f6f922b8f63aa86d6d3da
kasirait <159796564+kasirait@users.noreply.github.com>
2024-03-27 08:57:50 +0300

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+using namespace std;
+double mypow(float num1,float num2) {//возведение числа в степень
+    double num = 1;
+    for (int i = 0; i < num2; i++) {
+        num*= num1;
+    }
+    return num;
+}
+int summaInDiap(int num1, int num2) {//сумма чисел в их диапазоне
+    if (num1 > num2) swap(num1, num2);
+    int summa = 0;
+    for (int i = num1; i < num2 + 1; i++) {
+        summa += i;
+    }
+    return summa;
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    float num1, num2;
+    cin >> num1 >> num2;
+    cout << mypow(num1, num2)<<endl;//результат степени
+    cout << summaInDiap(num1, num2);//результат суммы
+
+
+}
