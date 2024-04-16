f11e3df3781f908fe6dbb6430362dc71492a7c2f
kasirait <159796564+kasirait@users.noreply.github.com>
2024-04-16 20:08:33 +0300

    Add files via upload

---
+++ dinmassiv.cpp
@@ -0,0 +1,20 @@
+﻿// abuabu.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    srand(time(NULL));
+    int size;
+    cin >> size;
+    int* array = new int[size];
+    cout << array << endl;
+    for (int i = 0; i < size; i++) {
+        array[i] = 1 + rand() % 9;
+        cout << array[i] << " ";
+    }
+}
+
