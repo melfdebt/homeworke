67555f812b04b1f655f3f46e081e776485484415
melfdebt <159796564+melfdebt@users.noreply.github.com>
2024-12-02 17:00:57 +0300

    Update Student.cpp

--- 25.11.24/Student.cpp
+++ 25.11.24/Student.cpp
@@ -1,1 +1,32 @@
 #include "Student.h"
+
+Student::Student(string fio, string login, string password): Human(fio,login,password)
+{
+	markLists = nullptr;
+}
+
+
+
+void Student::changePassword(string newPassword)
+{
+	cout << "are you sure?" << endl;
+	string yesNO;
+	cin >> yesNO;
+	if (yesNO == "Yes") {
+		this->password = newPassword;
+	}
+	else { return; }
+}
+
+MarkList* Student::getList(string subject)
+{
+	for (int i = 0; i < subjects; i++) {
+		if (markLists[i]->getSubject() == subject) {
+			return  markLists[i];
+		}
+	}
+	return nullptr;
+}
+
+
+
