a2b10dbaae2c7ec5840437ddb5c9a509de8c1926
melfdebt <159796564+melfdebt@users.noreply.github.com>
2024-12-02 16:57:50 +0300

    Update Academy.h

--- 25.11.24/Academy.h
+++ 25.11.24/Academy.h
@@ -7,6 +7,23 @@ class Academy//abstruct
 protected:
 	Teacher* teachers;
 	Group* groups;
-	Schedule schedule;
+	Schedule* schedule;
+	int teachersQuant;
+	int groupsQuant;
+	int groupsQuant;
+	int findGroupIndex(string name);
+public:
+	Academy(Teacher* teachers, Group* groups, Schedule* schedule);
+	~Academy();
+	Teacher* getTeachers();
+	Group* getGroups();
+	Schedule* getSchedule();
+	void changeSchedule(Schedule* newSched);
+	void deleteGroup(string name);
+	Group* findGroup(string name);
+	Teacher* findTeacher(string fio);
+	void deleteTeacher(string name);
+	void addTeacher(Teacher* teacher);
+	void addGroup(Group* group);
 };
 
