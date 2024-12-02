719d3e9b34b943663a89c1079d82cea220fb15da
melfdebt <159796564+melfdebt@users.noreply.github.com>
2024-12-02 16:59:27 +0300

    Update Lesson.h

--- 25.11.24/Lesson.h
+++ 25.11.24/Lesson.h
@@ -3,6 +3,7 @@
 #include "Group.h"
 #include <Time.h>
 #include <string.h>
+#include <map>
 using namespace std;
 class Lesson
 {
@@ -14,4 +15,10 @@ private:
 	time_t timeEnd;
 public:
 	Lesson(Teacher* teacher, Group* group, string subject, time_t time1, time_t time2);
+	Group* getGroup();
+	Teacher* getTeacher();
+	string getSubject();
+	pair<time_t, time_t> getTime();
+	void setTeacher(Teacher* teach);
+	void setTime(time_t time1, time_t time2);
 };
