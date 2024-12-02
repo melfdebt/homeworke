6b04f6f8b189475e9d2c13aa94371c686773647c
melfdebt <159796564+melfdebt@users.noreply.github.com>
2024-12-02 16:58:44 +0300

    Update Group.h

--- 25.11.24/Group.h
+++ 25.11.24/Group.h
@@ -5,8 +5,18 @@ using namespace std;
 class Group
 {
 private:
-	Student* student;
+	Student** students;
 	string name;
 	string faculty;
+	int quant;
+public:
+	Group(string faculty, string name, Student** students,int quant);
+	~Group();
+	void addStudent(Student* st);
+	void deleteStudent(string name);
+	int findSt(string name);
+	Student** getStudents();
+	string getName();
+	Student* getStudent(string name);
+	void showStudents();
 };
-
